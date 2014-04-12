45c8ed38ddf8ad37a3a0a9a8b7824656ae18c9d8
Glynn Forrest <me@glynnforrest.com>
2014-04-12 12:51:55 +0000

    Adding IntegerTest.

---
+++ tests/Reform/Tests/Validation/Rule/IntegerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Reform\Tests\Validation\Rule;
+
+require_once __DIR__ . '/../../../../bootstrap.php';
+
+use Reform\Validation\Rule\Integer;
+
+/**
+ * IntegerTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class IntegerTest extends RuleTest
+{
+
+    protected $rule;
+
+    public function setup()
+    {
+        $this->rule = new Integer();
+    }
+
+    public function dataProvider()
+    {
+        return array(
+            array(0, false),
+            array(1, true),
+            array(-1, true),
+            array('3', true),
+            array('-5', true),
+            array('foo', false),
+            array(4.5, false),
+        );
+    }
+
+}
