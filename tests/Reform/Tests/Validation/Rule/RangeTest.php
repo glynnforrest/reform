b203da152ca32545f3632c36c7a03e23b6c5157b
Glynn Forrest <me@glynnforrest.com>
2014-04-12 13:03:17 +0000

    Adding RangeTest.

---
+++ tests/Reform/Tests/Validation/Rule/RangeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Reform\Tests\Validation\Rule;
+
+require_once __DIR__ . '/../../../../bootstrap.php';
+
+use Reform\Validation\Rule\Range;
+
+/**
+ * RangeTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class RangeTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function dataProvider()
+    {
+        return array(
+            array(1, 5, 1, true),
+            array(0, 5, 1, true),
+            array(0, 5, 0, true),
+            array(-3, 5, 1, true),
+            array(-3, 5, -3, true),
+            array(0, 0, 0, true),
+            array(-9, -2, -3, true),
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider()
+     */
+    public function testRule($min, $max, $value, $pass)
+    {
+        $rule = new Range($min, $max);
+        $result = $this->getMock('Reform\Validation\Result');
+        if ($pass) {
+            $this->assertTrue($rule->validate($result, 'value', $value));
+        } else {
+            $this->assertFalse($rule->validate($result, 'value', $value));
+        }
+    }
+
+}
