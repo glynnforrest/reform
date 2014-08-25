d954b7633cff57127c48cde536892ee57d74ca8e
Glynn Forrest <me@glynnforrest.com>
2014-08-25 16:51:10 +0100

    Adding Checkbox.

---
+++ src/Reform/Form/Row/Checkbox.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Reform\Form\Row;
+
+use Reform\Helper\Html;
+
+/**
+ * Checkbox
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class Checkbox extends AbstractRow
+{
+
+    public function input()
+    {
+        if ($this->value !== null) {
+            $this->addAttributes(array('checked'));
+        }
+        //no matter what, the value of the input is 'checked'
+        $value = 'checked';
+
+        return Html::input('checkbox', $this->name, $value, $this->attributes);
+    }
+
+    public function render()
+    {
+        $str = str_replace(':label', $this->label(), $this->row_string);
+        $str = str_replace(':error', $this->error(), $str);
+        $str = str_replace(':input', $this->input(), $str);
+
+        return $str;
+    }
+
+}
