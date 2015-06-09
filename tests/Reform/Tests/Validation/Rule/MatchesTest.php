20c4fa1ddc4e712e2037ca3f183445c6004d0904
Glynn Forrest <me@glynnforrest.com>
2015-06-09 09:20:38 +0100

    Adding test for Matches rule.

---
+++ tests/Reform/Tests/Validation/Rule/MatchesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Reform\Tests\Validation\Rule;
+
+use Reform\Validation\Rule\Matches;
+
+/**
+ * MatchesTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class MatchesTest extends \PHPUnit_Framework_TestCase
+{
+    protected $rule;
+
+    public function setup()
+    {
+        $this->rule = new Matches('second');
+    }
+
+    public function testMatchingValues()
+    {
+        $result = $this->getMock('Reform\Validation\Result');
+        $this->assertTrue($this->rule->validate($result, 'first', 'foo', array('second' => 'foo')));
+    }
+
+    public function testNotMatchingValues()
+    {
+        $result = $this->getMock('Reform\Validation\Result');
+        $this->assertFalse($this->rule->validate($result, 'first', 'foo', array('second' => 'bar')));
+    }
+}
